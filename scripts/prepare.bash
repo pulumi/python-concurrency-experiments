f1e6132ed41bd076d2fd7be804b4fa2d0a1c6414
Robbie McKinstry <thesnowmancometh@gmail.com>
2022-10-21 18:17:11 -0400

    Build code from scratch each time.

--- scripts/prepare.bash
+++ scripts/prepare.bash
@@ -6,10 +6,44 @@ readonly PULUMI=/opt/homebrew/bin/pulumi
 readonly PULUMI_DEV=/Users/robbiemckinstry/.pulumi-dev/bin/pulumi
 readonly PROJECT_DIR="$1"
 
+function build_python_runtime() {
+    pushd $(pwd)
+    cd ~/workspace/pulumi/pulumi/sdk/python
+    make ensure && make build && make install
+    popd
+}
+
+function checkout_master() {
+    pushd $(pwd)
+    cd ~/workspace/pulumi/pulumi
+    git checkout master
+    make ensure && make build && make install
+    popd
+}
+
+function checkout_branch() {
+    pushd $(pwd)
+    cd ~/workspace/pulumi/pulumi
+    git checkout mckinstry/python-concurrency
+    make ensure && make build && make install
+    popd
+}
+
 function main {
+  PULUMI_EXEC=""
+  if [ "${PROJECT_DIR}" == "control" ]
+  then
+    PULUMI_EXEC="${PULUMI}"
+    checkout_master
+    build_python_runtime
+  else
+    PULUMI_EXEC="${PULUMI_DEV}"
+    checkout_branch
+    build_python_runtime
+  fi
   pushd $(pwd)
   cd "${PROJECT_DIR}"
-  $PULUMI destroy --yes --skip-preview || true
+  $PULUMI_EXEC destroy --yes --non-interactive --stack=dev --skip-preview || true
   popd
 }
 
--- scripts/up.bash
+++ scripts/up.bash
@@ -5,9 +5,20 @@ set -exuo pipefail
 readonly PULUMI=/opt/homebrew/bin/pulumi
 readonly PULUMI_DEV=/Users/robbiemckinstry/.pulumi-dev/bin/pulumi
 readonly PROJECT_DIR="$1"
+# readonly PULUMI_HOME_CONTROL=/Users/robbiemckinstry/.pulumi/
+# readonly PULUMI_HOME_DEV=/Users/robbiemckinstry/.pulumi-dev/
 
 function run_project {
-  $PULUMI up --yes --skip-preview
+  local PULUMI_EXEC=""
+  if [ "${PROJECT_DIR}" == "control" ]
+  then
+    PULUMI_EXEC="${PULUMI}"
+    # export PULUMI_HOME="${PULUMI_HOME_CONTROL}"
+  else
+    PULUMI_EXEC="${PULUMI_DEV}"
+    # export PULUMI_HOME="${PULUMI_HOME_DEV}"
+  fi
+  $PULUMI_EXEC up --yes --non-interactive --stack=dev --skip-preview
 }
 
 function main {
