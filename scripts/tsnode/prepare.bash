c4814dc260ca98b85c9395b57b87d52fb88f1563
Robbie McKinstry <robbie@pulumi.com>
2023-04-05 17:28:31 -0400

    Detach the branch

--- scripts/tsnode/prepare.bash
+++ scripts/tsnode/prepare.bash
@@ -19,7 +19,7 @@ function build_node_runtime() {
 function checkout_master() {
     pushd $(pwd)
     cd "${WORKSPACE}"
-    git switch "origin/master"
+    git switch "origin/master" --detach
     make ensure && make build && make install
     popd
 }
@@ -27,7 +27,7 @@ function checkout_master() {
 function checkout_slow() {
     pushd $(pwd)
     cd "${WORKSPACE}"
-    git switch "origin/mckinstry/ts-node-expected-slow"
+    git switch "origin/mckinstry/ts-node-expected-slow" --detach
     make ensure && make build && make install
     popd
 }
@@ -35,7 +35,7 @@ function checkout_slow() {
 function checkout_tsnode() {
     pushd $(pwd)
     cd "${WORKSPACE}"
-    git switch "origin/mckinstry/perf-test-ts-node"
+    git switch "origin/mckinstry/perf-test-ts-node" --detach
     make ensure && make build && make install
     popd
 }
